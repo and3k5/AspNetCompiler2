1bb44259b0bc4dc327296cb122cd53adaa82bd97
Anders K <and3k5@users.noreply.github.com>
2023-05-03 14:59:57 +0200

    Add project

---
+++ AspNetCompiler2/AspNetCompiler2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Build.Tasks;
+
+namespace AspNetCompiler2
+{
+    // ReSharper disable once UnusedMember.Global
+    public class AspNetCompiler2 : AspNetCompiler
+    {
+        public string Exclude
+        {
+            get => (string) Bag[nameof(Exclude)];
+            set => Bag[nameof(Exclude)] = value;
+        }
+
+        public string ExcludeSeparator
+        {
+            get => (string) Bag[nameof(ExcludeSeparator)];
+            set => Bag[nameof(ExcludeSeparator)] = value;
+        }
+
+        protected string[] GetExcluded()
+        {
+            if (string.IsNullOrWhiteSpace(Exclude))
+                return new string[0];
+
+            return !string.IsNullOrEmpty(ExcludeSeparator)
+                ? Exclude.Split(new[] {ExcludeSeparator}, StringSplitOptions.RemoveEmptyEntries)
+                : new[] {Exclude};
+        }
+
+        protected override void AddCommandLineCommands(CommandLineBuilderExtension commandLine)
+        {
+            base.AddCommandLineCommands(commandLine);
+
+            foreach (var path in GetExcluded())
+                commandLine.AppendSwitchIfNotNull("-x ", path);
+        }
+    }
+}
---
+++ AspNetCompiler2/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AspNetCompiler2")]
+[assembly: AssemblyDescription("An extension to the original AspNetCompiler since MS didn't want to add the exclusion parameter into the task.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Anders Kjeldsen")]
+[assembly: AssemblyProduct("AspNetCompiler2")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4d3bc090-d2c3-4b28-8b04-911e9ba8baa5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
